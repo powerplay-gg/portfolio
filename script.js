90ce71ccabc16ab5c435bc400441249686ca7367
powerplay-gg <chaplygindesign@gmail.com>
2026-01-03 10:08:31 +0300

    redesign
    new design

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 window.addEventListener("load", function () {
-  document.querySelector(".burger").addEventListener("click", function () {
-    this.classList.toggle("active");
-    console.log(this.classList);
-    document.querySelector(".leftbar").classList.toggle("open");
-  });
+
   var lightbox = new SimpleLightbox(".lightbox", {
-    close: false, nav: false, showCounter: false
+    close: false,
+    nav: false,
+    showCounter: false
   });
 });
